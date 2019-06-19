66e8a725f432bf3d881c216df701af9d12fd12c5
nadeeraka <nimanthanadeeraka@outlook.com>
2019-06-19 16:41:45 +0530

    bootswatch added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./App.css";
+import "bootswatch/dist/lux/bootstrap.css";
+import NavBar from "./components/layouts/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <p>jjjj</p>
     </div>
   );

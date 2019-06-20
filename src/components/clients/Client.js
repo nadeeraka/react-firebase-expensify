e2cdb0aa48c43f849e02c1a0a85ac1593c95769a
nadeeraka <nimanthanadeeraka@outlook.com>
2019-06-20 13:08:20 +0530

    style added

--- src/components/clients/Client.js
+++ src/components/clients/Client.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-
+import uuid from "uuid";
+import { Link } from "react-router-dom";
 class Client extends Component {
   render() {
     const client = [
@@ -12,7 +13,47 @@ class Client extends Component {
       }
     ];
     if (client) {
-      return <h1>Clients</h1>;
+      return (
+        <div className="row">
+          <div className="col-md-6">
+            <h2>
+              {" "}
+              <i className="fas fa-users" /> Clients
+            </h2>
+          </div>
+          <div className="col-md-6">
+            <table className="table table-striped">
+              <thead className="thead-inverse">
+                <tr>
+                  <th> Name</th>
+
+                  <th>Email</th>
+                  <th>Balance</th>
+                </tr>
+              </thead>
+              {client.map(client => (
+                <tr key={uuid()}>
+                  <td>
+                    {client.firstName} {client.lastName}
+                  </td>
+
+                  <td>{client.email}</td>
+                  <td>{client.balance}</td>
+                  <td>
+                    {" "}
+                    <Link
+                      to={`client/${client.id}`}
+                      className="btn btn-secondary btn-sm"
+                    >
+                      Details
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </table>
+          </div>
+        </div>
+      );
     } else {
       return <h3>Loding....</h3>;
     }
